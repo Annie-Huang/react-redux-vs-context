d15fadcad56c1cf2ff5098579cdee88288abe015
Annie Huang <annie.hn@gmail.com>
2019-04-20 23:05:03 +1000

    Cut addProductToCart and removeProductFromCart to place in reducers.js

--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useReducer } from 'react';
 
 import ShopContext from './shop-context';
 
@@ -12,48 +12,6 @@ const GlobalState = props => {
   ];
 
   const [cart, setCart] = useState([]);
-
-  const addProductToCart = product => {
-    console.log('Adding product', product);
-    const updatedCart = [...cart];
-    const updatedItemIndex = updatedCart.findIndex(
-      item => item.id === product.id
-    );
-
-    if (updatedItemIndex < 0) {
-      updatedCart.push({ ...product, quantity: 1 });
-    } else {
-      const updatedItem = {
-        ...updatedCart[updatedItemIndex]
-      };
-      updatedItem.quantity++;
-      updatedCart[updatedItemIndex] = updatedItem;
-    }
-    setTimeout(() => {
-      setCart(updatedCart);
-    }, 700);
-  };
-
-  const removeProductFromCart = productId => {
-    console.log('Removing product with id: ' + productId);
-    const updatedCart = [...cart];
-    const updatedItemIndex = updatedCart.findIndex(
-      item => item.id === productId
-    );
-
-    const updatedItem = {
-      ...updatedCart[updatedItemIndex]
-    };
-    updatedItem.quantity--;
-    if (updatedItem.quantity <= 0) {
-      updatedCart.splice(updatedItemIndex, 1);
-    } else {
-      updatedCart[updatedItemIndex] = updatedItem;
-    }
-    setTimeout(() => {
-      setCart(updatedCart);
-    }, 700);
-  };
 
   return (
     <ShopContext.Provider
