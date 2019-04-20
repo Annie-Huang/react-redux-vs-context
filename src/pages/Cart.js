b3ec22519c6bd21239384c8e1f6147be5c001ca9
Annie Huang <annie.hn@gmail.com>
2019-04-20 20:24:20 +1000

    (Will not run), First change the Cart.js into functional component.

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -6,48 +6,47 @@ import MainNavigation from '../components/MainNavigation';
 // import { removeProductFromCart } from '../store/actions';
 import './Cart.css';
 
-class CartPage extends Component {
-  static contextType = ShopContext;
+const CartPage = props =>  {
+  // static contextType = ShopContext;
+  //
+  // componentDidMount() {
+  //   console.log(this.context);
+  // }
 
-  componentDidMount() {
-    console.log(this.context);
-  }
+  return (
+    <React.Fragment>
+    <MainNavigation
+      cartItemNumber={this.context.cart.reduce((count, curItem) => {
+        return count + curItem.quantity;
+      }, 0)}
+    />
+    <main className="cart">
+      {this.context.cart.length <= 0 && <p>No Item in the Cart!</p>}
+      <ul>
+        {this.context.cart.map(cartItem => (
+          <li key={cartItem.id}>
+            <div>
+              <strong>{cartItem.title}</strong> - ${cartItem.price} (
+              {cartItem.quantity})
+            </div>
+            <div>
+              <button
+                onClick={this.context.removeProductFromCart.bind(
+                  this,
+                  cartItem.id
+                )}
+              >
+                Remove from Cart
+              </button>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </main>
+    </React.Fragment>
+  );
 
-  render() {
-    return (
-      <React.Fragment>
-        <MainNavigation
-          cartItemNumber={this.context.cart.reduce((count, curItem) => {
-            return count + curItem.quantity;
-          }, 0)}
-        />
-        <main className="cart">
-          {this.context.cart.length <= 0 && <p>No Item in the Cart!</p>}
-          <ul>
-            {this.context.cart.map(cartItem => (
-              <li key={cartItem.id}>
-                <div>
-                  <strong>{cartItem.title}</strong> - ${cartItem.price} (
-                  {cartItem.quantity})
-                </div>
-                <div>
-                  <button
-                    onClick={this.context.removeProductFromCart.bind(
-                      this,
-                      cartItem.id
-                    )}
-                  >
-                    Remove from Cart
-                  </button>
-                </div>
-              </li>
-            ))}
-          </ul>
-        </main>
-      </React.Fragment>
-    );
-  }
-}
+};
 
 // const mapStateToProps = state => {
 //   return {
